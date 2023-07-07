94d60a2c51056d45468afecd70d8b19efc5edab4
MirMoheen12 <mirmoheen9@gmail.com>
2023-07-07 17:06:54 +0500

    Setup code

---
+++ CTW_FIA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CTW_FIA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
