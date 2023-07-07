899b765c59f0f8ac1d7b3b3efd90d41afbee98cd
MirMoheen12 <mirmoheen9@gmail.com>
2023-07-07 17:52:37 +0500

    Setup Identity and packages

---
+++ CTW_FIA/Models/DatabaseModels/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace CTW_FIA.Models.DatabaseModels
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+          : base(options)
+        {
+
+        }
+        //public DbSet<Paymencategory> Paymencategory { get; set; }
+
+    }
+}
