c26c93fbc9a2ae116ed71fceaf7ce561ad9e41bc
mirmoheen9 <mirmoheen9@gmail.com>
2023-07-08 15:32:05 +0500

    commiting

---
+++ CTW_FIA/Controllers/incidentsController.cs
@@ -0,0 +1,26 @@
+﻿using CTW_FIA.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTW_FIA.Controllers
+{
+    [AllowAnonymous]
+    public class incidentsController : Controller
+    {
+        private readonly IDashboard dashboard;
+        public incidentsController(IDashboard dashboard)
+        {
+            this.dashboard = dashboard;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public JsonResult Getdashboard()
+        {
+            var data = dashboard.getCtwdashboardsGraphwise();
+            return Json(data);
+        }
+    }
+}
