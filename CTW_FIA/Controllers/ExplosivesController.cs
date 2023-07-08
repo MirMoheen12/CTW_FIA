d0499aa6260a852729251b5dc416eec309181375
mirmoheen9 <mirmoheen9@gmail.com>
2023-07-08 16:55:03 +0500

    dashboards

---
+++ CTW_FIA/Controllers/ExplosivesController.cs
@@ -0,0 +1,27 @@
+﻿using CTW_FIA.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTW_FIA.Controllers
+{
+    [AllowAnonymous]
+    public class ExplosivesController : Controller
+    {
+        private readonly IDashboard dashboard;
+        public ExplosivesController(IDashboard dashboard)
+        {
+            this.dashboard = dashboard;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public JsonResult Getdashboard()
+        {
+            var data = dashboard.getCtwdashboardsGraphwise();
+            return Json(data);
+        }
+
+    }
+}
---
+++ CTW_FIA/Controllers/TerroristsController.cs
@@ -0,0 +1,27 @@
+﻿using CTW_FIA.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTW_FIA.Controllers
+{
+    [AllowAnonymous]
+    public class TerroristsController : Controller
+    {
+
+        private readonly IDashboard dashboard;
+        public TerroristsController(IDashboard dashboard)
+        {
+            this.dashboard = dashboard;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public JsonResult Getdashboard()
+        {
+            var data = dashboard.getCtwdashboardsGraphwise();
+            return Json(data);
+        }
+    }
+}
