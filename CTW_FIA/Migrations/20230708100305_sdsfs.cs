812f83a6eb637756be86d765d16348ff989d7bba
MirMoheen12 <mirmoheen9@gmail.com>
2023-07-08 15:30:12 +0500

    test changes

---
+++ CTW_FIA/Migrations/20230708100305_sdsfs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTW_FIA.Migrations
+{
+    public partial class sdsfs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CTW_FIA/Migrations/20230708100721_ksdsjs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTW_FIA.Migrations
+{
+    public partial class ksdsjs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
