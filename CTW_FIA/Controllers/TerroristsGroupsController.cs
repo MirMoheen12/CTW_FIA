b1e6ad247954d8f274c6ac7cc08c727a5b3f6ef4
mirmoheen9 <mirmoheen9@gmail.com>
2023-07-08 11:30:29 +0500

    terroristsgroup

--- CTW_FIA/Controllers/TerroristsGroupsController.cs
+++ CTW_FIA/Controllers/TerroristsGroupsController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CTW_FIA.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CTW_FIA.Controllers
 {
     public class TerroristsGroupsController : Controller
     {
+        private readonly IDashboard dashboard;
+        public TerroristsGroupsController(IDashboard dashboard)
+        {
+            this.dashboard = dashboard;
+        }
         public IActionResult Index()
         {
+            //var data = dashboard.getCtwdashboard();
             return View();
         }
+        public JsonResult Getdashboard()
+        {
+            var data = dashboard.getCtwdashboard();
+            return Json(data);
+        }
+
+
     }
 }
