3df68b1a36f7b2a7b5ba86ac8ae0fd8dc95c3c6b
MirMoheen12 <mirmoheen9@gmail.com>
2023-07-09 15:04:31 +0500

    Setup Entry form

---
+++ CTW_FIA/Controllers/RecordController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTW_FIA.Controllers
+{
+    [AllowAnonymous]
+    public class RecordController : Controller
+    {
+        public IActionResult AddRecord()
+        {
+            return View();
+        }
+    }
+}
