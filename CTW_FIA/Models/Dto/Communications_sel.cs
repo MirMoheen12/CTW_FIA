2f6b708832ed7b4cb270f2175f9e222da1eec04a
MirMoheen12 <mirmoheen9@gmail.com>
2023-07-16 22:30:21 +0500

    Added class

---
+++ CTW_FIA/Models/Dto/Communications_sel.cs
@@ -0,0 +1,21 @@
+﻿namespace CTW_FIA.Models.Dto
+{
+    public class Communications_sel
+    {
+        public string? SerialNo { get; set; }
+        public string? strURN { get; set; }
+        public string? intID { get; set; }
+        public string? CommType { get; set; }
+        public string? CommDescription { get; set; }
+        public string? CommName { get; set; }
+        public string? Other { get; set; }
+        public string? WebValue { get; set; }
+        public string? Number { get; set; }
+        public string? ServiceProvider { get; set; }
+        public string? CountryOrigin { get; set; }
+        public string? memRemarks { get; set; }
+        public string? CreatedOn { get; set; }
+        public string? DateSent { get; set; }
+        public string? IsDeleted { get; set; }
+    }
+}
---
+++ CTW_FIA/Models/Dto/Vehicles_sel_Result.cs
@@ -0,0 +1,21 @@
+﻿namespace CTW_FIA.Models.Dto
+{
+    public class Vehicles_sel_Result
+    {
+        public string? SerialNo { get; set; }
+        public string? strURN { get; set; }
+        public string? intID { get; set; }
+        public string? VehicleType { get; set; }
+        public string? VehicleSubType { get; set; }
+        public string? MakeModel { get; set; }
+        public string? RegistrationNo { get; set; }
+        public string? ChasesNo { get; set; }
+        public string? Color { get; set; }
+        public string? Size { get; set; }
+        public string? DeviceLocation { get; set; }
+        public string? memRemarks { get; set; }
+        public string? dteSent { get; set; }
+        public string? CreatedOn { get; set; }
+        public string? CreatedBy { get; set; }
+    }
+}
