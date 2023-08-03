2a6281b4e0f0d361f43306adb8d6c8252af10a51
MirMoheen12 <mirmoheen9@gmail.com>
2023-08-03 21:57:53 +0500

    custmozed details

---
+++ CTW_FIA/Models/LocalModels/DisplayModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CTW_FIA.Models.LocalModels
+{
+    public class DisplayModel
+    {
+        public string? PropertName { get; set; }
+        public string? PropertValue { get; set; }
+    }
+}
