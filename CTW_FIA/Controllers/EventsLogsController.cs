8e710132765e2a66268a66c8da5aeadcd1ad629d
MirMoheen12 <mirmoheen9@gmail.com>
2023-08-28 00:58:55 +0500

    kjdks

---
+++ CTW_FIA/Controllers/EventsLogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CTW_FIA.Controllers
+{
+    public class EventsLogsController : Controller
+    {
+        public IActionResult AllLogs()
+        {
+            return View();
+        }
+    }
+}
